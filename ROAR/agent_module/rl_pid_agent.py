aa5370c051d12df699f52bc5620a0c395e70eb72
Tianlun <ztl1998@berkeley.edu>
2021-10-14 23:45:48 -0700

    update pid

--- ROAR/agent_module/rl_pid_agent.py
+++ ROAR/agent_module/rl_pid_agent.py
@@ -1,6 +1,6 @@
 from ROAR.agent_module.agent import Agent
 from pathlib import Path
-from ROAR.control_module.rl_pid_controller import PIDController
+from ROAR.control_module.pid_controller import PIDController
 from ROAR.planning_module.local_planner.simple_waypoint_following_local_planner import \
     SimpleWaypointFollowingLocalPlanner
 from ROAR.planning_module.behavior_planner.behavior_planner import BehaviorPlanner
